40710e076488156622612bfc81c2e6e11c3d3eda
Stanley Rafael <stanleyrafael1710@yahoo.com>
2025-02-17 17:09:46 +0700

    Installer update

--- installer.bat
+++ installer.bat
@@ -1,12 +1,12 @@
 @echo off
 cd %~dp0
-echo Setting up virtual environment...
+echo Setting up virtual environment..
 python -m venv venv
 
-echo Activating virtual environment...
+echo Activating virtual environment..
 call venv\Scripts\activate
 
-echo Installing dependencies from requirements.txt...
+echo Installing dependencies from requirements.txt..
 pip install -r requirements.txt
 
 echo Setup complete! You can now proceed to run database migrations.
